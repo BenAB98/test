853e7799181f0673053c7ca9b07b1e5abe3eaa4d
BenAB98 <behnamabolfathi98@gmail.com>
2022-01-22 03:06:31 +0330

    index.js added

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hi Saheb");
+alert("shir")
+console.log("Hello there");
+console.log('hi saheb');
+console.log("hi saheb");
+console.log('hi saheb');
+console.log('hi saheb');
+console.log("hi saheb");
+console.log('hi saheb');
